654bca80c8bcd91d7702064a7626e06f34a97d2e
bluluke <bluluke@hotmail.com>
2024-02-06 15:00:15 +0000

    Functionality: stream database credentials from database.properties file in App.java.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,35 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static String jdbcUrl;
+    private static String username;
+    private static String password;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+
+
+         Properties properties = new Properties();
+        String filePath = "src/main/resources/database.properties";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            properties.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        jdbcUrl = properties.getProperty("db.url");
+        username = properties.getProperty("db.username");
+        password = properties.getProperty("db.password");
     }
 }
